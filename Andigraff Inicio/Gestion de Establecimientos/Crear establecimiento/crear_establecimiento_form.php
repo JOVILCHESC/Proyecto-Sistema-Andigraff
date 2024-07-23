2fcdc06e7b30cb79c07780ff8cb9e3761abec570
joaquinortiz1 <joaquin.ortiz.que@gmail.com>
2024-07-22 20:44:14 -0400

    cambio css

--- Andigraff Inicio/Gestion de Establecimientos/Crear establecimiento/crear_establecimiento_form.php
+++ Andigraff Inicio/Gestion de Establecimientos/Crear establecimiento/crear_establecimiento_form.php
@@ -5,53 +5,71 @@
     <link rel="stylesheet" href="../styles/crear_establecimiento.css">
 </head>
 <body>
-    <h1>Registro de Establecimiento</h1>
-    <form action="./crear_establecimiento.php" method="post">
-        <label for="telefono">Teléfono:</label>
-        <input type="text" id="telefono" name="telefono" required><br>
+    <div class="container">
+        <h1>Registro de Establecimiento</h1>
+        <form action="./crear_establecimiento.php" method="post">
+            <div class="form-group">
+                <label for="telefono">Teléfono:</label>
+                <input type="text" id="telefono" name="telefono" required>
+            </div>
 
-        <label for="numero_estableciimiento">Número Establecimiento:</label>
-        <input type="text" id="numero_estableciimiento" name="numero_estableciimiento" required><br>
+            <div class="form-group">
+                <label for="numero_estableciimiento">Número Establecimiento:</label>
+                <input type="text" id="numero_estableciimiento" name="numero_estableciimiento" required>
+            </div>
 
-        <label for="comuna_establecimiento">Comuna:</label>
-        <input type="text" id="comuna_establecimiento" name="comuna_establecimiento" required><br>
+            <div class="form-group">
+                <label for="comuna_establecimiento">Comuna:</label>
+                <input type="text" id="comuna_establecimiento" name="comuna_establecimiento" required>
+            </div>
 
-        <label for="calle_establecimiento">Calle:</label>
-        <input type="text" id="calle_establecimiento" name="calle_establecimiento" required><br>
+            <div class="form-group">
+                <label for="calle_establecimiento">Calle:</label>
+                <input type="text" id="calle_establecimiento" name="calle_establecimiento" required>
+            </div>
 
-        <label for="ciudad_establecimiento">Ciudad:</label>
-        <input type="text" id="ciudad_establecimiento" name="ciudad_establecimiento" required><br>
+            <div class="form-group">
+                <label for="ciudad_establecimiento">Ciudad:</label>
+                <input type="text" id="ciudad_establecimiento" name="ciudad_establecimiento" required>
+            </div>
 
-        <label for="nombre_establecimiento">Nombre Establecimiento:</label>
-        <input type="text" id="nombre_establecimiento" name="nombre_establecimiento" required><br>
+            <div class="form-group">
+                <label for="nombre_establecimiento">Nombre Establecimiento:</label>
+                <input type="text" id="nombre_establecimiento" name="nombre_establecimiento" required>
+            </div>
 
-        <label for="cant_empleados">Cantidad de Empleados:</label>
-        <input type="number" id="cant_empleados" name="cant_empleados" required><br>
+            <div class="form-group">
+                <label for="cant_empleados">Cantidad de Empleados:</label>
+                <input type="number" id="cant_empleados" name="cant_empleados" required>
+            </div>
 
-        <label for="tipo">Tipo de Establecimiento:</label>
-        <select id="tipo" name="tipo" required>
-            <option value="sucursal">Sucursal</option>
-            <option value="bodega">Bodega</option>
-        </select><br>
+            <div class="form-group">
+                <label for="tipo">Tipo de Establecimiento:</label>
+                <select id="tipo" name="tipo" required>
+                    <option value="sucursal">Sucursal</option>
+                    <option value="bodega">Bodega</option>
+                </select>
+            </div>
 
-        <div id="sucursal_fields" style="display: none;">
-            <label for="tipo_sucursal">Tipo de Sucursal:</label>
-            <input type="text" id="tipo_sucursal" name="tipo_sucursal"><br>
-        </div>
+            <div id="sucursal_fields" class="form-group" style="display: none;">
+                <label for="tipo_sucursal">Tipo de Sucursal:</label>
+                <input type="text" id="tipo_sucursal" name="tipo_sucursal">
+            </div>
 
-        <div id="bodega_fields" style="display: none;">
-            <label for="capacidad">Capacidad:</label>
-            <input type="number" id="capacidad" name="capacidad"><br>
+            <div id="bodega_fields" class="form-group" style="display: none;">
+                <label for="capacidad">Capacidad:</label>
+                <input type="number" id="capacidad" name="capacidad"><br>
 
-            <label for="tipo_almacenamiento">Tipo de Almacenamiento:</label>
-            <input type="text" id="tipo_almacenamiento" name="tipo_almacenamiento"><br>
+                <label for="tipo_almacenamiento">Tipo de Almacenamiento:</label>
+                <input type="text" id="tipo_almacenamiento" name="tipo_almacenamiento"><br>
 
-            <label for="estado_bodega">Estado de Bodega:</label>
-            <input type="checkbox" id="estado_bodega" name="estado_bodega" value="1"><br>
-        </div>
+                <label for="estado_bodega">Estado de Bodega:</label>
+                <input type="checkbox" id="estado_bodega" name="estado_bodega" value="1">
+            </div>
 
-        <input type="submit" value="Registrar">
-    </form>
+            <button type="submit">Registrar</button>
+        </form>
+    </div>
 
     <script>
         document.getElementById('tipo').addEventListener('change', function () {
@@ -71,3 +89,4 @@
     </script>
 </body>
 </html>
+
